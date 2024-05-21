b783d43b8435800973ba77203aa0498c6e8bb4a5
jerrister <jerrister.150329@sjtu.edu.cn>
2024-05-21 11:02:09 +0800

    search v2: except Update time

--- lime_v1/src/services/tags.js
+++ lime_v1/src/services/tags.js
@@ -9,19 +9,38 @@ export async function loader(selectedValues, Content){
     const cur_tag = selectedValues[selectedValues.length-1];
     console.log("Selected Params for the loader are:", cur_tag);
     // const query = 'MATCH (p:Paper) RETURN p LIMIT 100';
-    const query = `
-    MATCH (p:Paper)-[:BELONGS_TO]->(parent:Tag)-[:IN*0..]->(root:Tag {tag_name: $tag})
-    RETURN p
-    `;
-    const result = await Neo4jAsk(query, {tag : cur_tag})
-    console.log("in dataloader, select values:",  result);
+    
 
     let reference_list = []
     console.log("Content:" , Content);
     console.log("Content Bool:" , Content === "Paper");
 
-    if (Content === "Paper")
+    if (Content === "Notebook")
     {
+        const query = `
+            MATCH (n:Note)-[:BELONGS_TO]->(parent:Tag)-[:IN*0..]->(root:Tag {tag_name: $tag})
+            RETURN DISTINCT n
+            `;
+        const result = await Neo4jAsk(query, {tag : cur_tag})
+        console.log("in dataloader, select values:",  result);
+        reference_list = result.map(record => {
+            const node = record.get('n');  // 获取节点
+            // console.log(node.properties);
+            return {
+                title: node.properties.name,
+                path: node.properties.path,
+                "build time": "2024/05/21 10:36:30",
+                };
+        })
+    }
+    else
+    {
+        const query = `
+            MATCH (p:Paper)-[:BELONGS_TO]->(parent:Tag)-[:IN*0..]->(root:Tag {tag_name: $tag})
+            RETURN DISTINCT p
+            `;
+        const result = await Neo4jAsk(query, {tag : cur_tag})
+        console.log("in dataloader, select values:",  result);
         reference_list = result.map(record => {
             const node = record.get('p');  // 获取节点
             // console.log(node.properties);
@@ -42,14 +61,6 @@ export async function loader(selectedValues, Content){
                     }
         })
     }
-    else
-    {
-        reference_list = [
-            {"title": "Title0" , "build time": '2024/5/19 16:25:18', 'path': '/'},
-            {"title": "Title1" , "build time": '2024/5/19 16:25:19', 'path': '/'},
-            {"title": "Title2" , "build time": '2024/5/19 16:25:17', 'path': '/'},
-        ]
-    }
 
     // const reference_list = [
     //     {"title": "Title0", "year": 2022, "source":"http"  }, 
@@ -64,13 +75,57 @@ export async function loader(selectedValues, Content){
 }
 
 
-export async function searcher(selectedValues, requestData){
+export async function searcher(selectedValues, Content, requestData){
 
     console.log("Params for the loader are:", selectedValues, requestData);
+    console.log("Content:" , Content);
+    console.log("Content Bool:" , Content === "Paper");
 
     var query = ``;
     var params = {};
 
+    let reference_list = []
+    console.log("Content:" , Content);
+    console.log("Content Bool:" , Content === "Paper");
+
+    if (Content === "Notebook")
+    {
+        if (requestData.searchField === "undefined") {
+            query = `
+            MATCH (n:Note)
+            RETURN n
+            `;
+            params = {};
+        }
+        else if (requestData.searchField === "Tag") {
+            query = `
+            MATCH (n:Note)-[:BELONGS_TO]->(parent:Tag)-[:IN*0..]->(root:Tag {tag_name: $tag})
+            RETURN DISTINCT n
+            `;
+            params = {tag : requestData.searchValue};
+        }
+        else if (requestData.searchField == "Title") {
+            query = `
+            MATCH (n:Note) WHERE n.name=~$name
+            RETURN DISTINCT n
+            `;
+            params = {name : `(?i).*${requestData.searchValue}.*`};
+        }
+
+
+        const result = await Neo4jAsk(query, params)
+        console.log("in dataloader, select values:",  result);
+        reference_list = result.map(record => {
+            const node = record.get('n');  // 获取节点
+            // console.log(node.properties);
+            return {
+                title: node.properties.name,
+                path: node.properties.path,
+                "build time": "2024/05/21 10:36:30",
+                };
+        })
+    }
+    else{
     if (requestData.searchField === "undefined") {
         query = `
         MATCH (p:Paper)
@@ -81,7 +136,7 @@ export async function searcher(selectedValues, requestData){
     else if (requestData.searchField === "Tag") {
         query = `
         MATCH (p:Paper)-[:BELONGS_TO]->(parent:Tag)-[:IN*0..]->(root:Tag {tag_name: $tag})
-        RETURN p
+        RETURN DISTINCT p
         `;
         params = {tag : requestData.searchValue};
     }
@@ -89,27 +144,20 @@ export async function searcher(selectedValues, requestData){
         query = `
         MATCH (p:Paper)-[:WRITTEN_BY]->(a:Author)
         WHERE a.name =~ $author
-        RETURN p
+        RETURN DISTINCT p
         `;
         params = {author : `(?i).*${requestData.searchValue}.*`};
     }
     else if (requestData.searchField == "Title") {
         query = `
         MATCH (p:Paper) WHERE p.title=~$title
-        RETURN p
+        RETURN DISTINCT p
         `;
         params = {title : `(?i).*${requestData.searchValue}.*`};
     }
 
-    // const cur_tag = selectedValues[selectedValues.length-1];
-    // console.log("Selected Params for the loader are:", cur_tag);
-    // // const query = 'MATCH (p:Paper) RETURN p LIMIT 100';
-    // const query = `
-    // MATCH (p:Paper)-[:BELONGS_TO]->(parent:Tag)-[:IN*0..]->(root:Tag {tag_name: $tag})
-    // RETURN p
-    // `;
     const result = await Neo4jAsk(query, params)
-    const reference_list = result.map(record => {
+    reference_list = result.map(record => {
             const node = record.get('p');  // 获取节点
             // console.log(node.properties);
             if (typeof(node.properties.year) == "string") {
@@ -127,4 +175,10 @@ export async function searcher(selectedValues, requestData){
                 };
             }
         })
+    };
+    
+    return new Map([
+            ['reference_list', reference_list],
+            ['BigTag',  selectedValues[0]],
+          ]);
     }
